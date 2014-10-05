abd8dc3e88480c1fd9690174ec60025093d7470f
Andrew Blakey <ablakey@ubuntu.(none)>
2014-10-04 23:25:43 -0400

    Initial commit.

---
+++ L.SimpleGraticule.js
@@ -0,0 +1,138 @@
+/**
+ *  File: L.SimpleGraticule.js
+ *  Desc: A graticule for Leaflet maps in the L.CRS.Simple coordinate system.
+ *  Auth: Andrew Blakey (ablakey@gmail.com)
+ */
+L.SimpleGraticule = L.LayerGroup.extend({
+    options: {
+        interval: 20,
+        showOriginLabel: true,
+        redraw: 'move'
+    },
+
+    lineStyle: {
+        stroke: true,
+        color: '#111',
+        opacity: 0.6,
+        weight: 1
+    },
+
+    initialize: function(options) {
+        L.LayerGroup.prototype.initialize.call(this);
+        L.Util.setOptions(this, options);
+    },
+
+    onAdd: function(map) {
+        this._map = map;
+
+        var graticule = this.redraw();
+        this._map.on('viewreset ' + options.redraw, graticule.redraw, graticule);
+
+        this.eachLayer(map.addLayer, map);
+    },
+
+    onRemove: function(map) {
+        map.off('viewreset '+ this.options.redraw, this.map);
+        this.eachLayer(this.removeLayer, this);
+    },
+
+    redraw: function() {
+        //1. get bounds with some padding
+        this._bounds = this._map.getBounds().pad(0.5);
+
+        this.clearLayers();
+
+        this.constructLines(this.getMins(), this.getLineCounts());
+
+        if (this.options.showOriginLabel) {
+            this.addLayer(this.addOriginLabel());
+        }
+        return this;
+    },
+
+    getLineCounts: function() {
+        return {
+            x: Math.ceil((this._bounds.getEast() - this._bounds.getWest()) /
+                this.options.interval),
+            y: Math.ceil((this._bounds.getNorth() - this._bounds.getSouth()) /
+                this.options.interval)
+        };
+    },
+
+    getMins: function() {
+        //rounds up to nearest multiple of x
+        var s = this.options.interval;
+        return {
+            x: Math.floor(this._bounds.getWest() / s) * s,
+            y: Math.floor(this._bounds.getSouth() / s) * s
+        };
+    },
+
+    constructLines: function(mins, counts) {
+        var lines = new Array(counts.x + counts.y);
+        var labels = new Array(counts.x + counts.y);
+
+        //for horizontal lines
+        for (var i = 0; i <= counts.x; i++) {
+            var x = mins.x + i * this.options.interval;
+            lines[i] = this.buildXLine(x);
+            labels[i] = this.buildLabel('gridlabel-horiz', x);
+        }
+
+        //for vertical lines
+        for (var j = 0; j <= counts.y; j++) {
+            var y = mins.y + j * this.options.interval;
+            lines[j + i] = this.buildYLine(y);
+            labels[j + i] = this.buildLabel('gridlabel-vert', y);
+        }
+
+        lines.forEach(this.addLayer, this);
+        labels.forEach(this.addLayer, this);
+    },
+
+    buildXLine: function(x) {
+        var bottomLL = new L.LatLng(this._bounds.getSouth(), x);
+        var topLL = new L.LatLng(this._bounds.getNorth(), x);
+
+        return new L.Polyline([bottomLL, topLL], this.lineStyle);
+    },
+
+    buildYLine: function(y) {
+        var leftLL = new L.LatLng(y, this._bounds.getWest());
+        var rightLL = new L.LatLng(y, this._bounds.getEast());
+
+        return new L.Polyline([leftLL, rightLL], this.lineStyle);
+    },
+
+    buildLabel: function(axis, val) {
+        var bounds = this._map.getBounds().pad(-0.003);
+        var latLng;
+        if (axis == 'gridlabel-horiz') {
+            latLng = new L.LatLng(bounds.getNorth(), val);
+        } else {
+            latLng = new L.LatLng(val, bounds.getWest());
+        }
+
+        return L.marker(latLng, {
+            icon: L.divIcon({
+                iconSize: [0, 0],
+                className: 'leaflet-grid-label',
+                html: '<div class="' + axis + '">' + val + '</div>'
+            })
+        });
+    },
+
+    addOriginLabel: function() {
+        return L.marker([0, 0], {
+            icon: L.divIcon({
+                iconSize: [0, 0],
+                className: 'leaflet-grid-label',
+                html: '<div class="gridlabel-horiz">(0,0)</div>'
+            })
+        });
+    }
+});
+
+L.simpleGraticule = function(options) {
+    return new L.SimpleGraticule(options);
+};
---
+++ README.md
@@ -0,0 +1,24 @@
+Leaflet.SimpleGraticule
+------------
+A graticule for maps in Leaflet's L.CRS.Simple coordinate system.Code inspiration came from Jan Pieter Waagmeester's fantastic
+[Leaflet.Grid](https://github.com/jieter) for world projections. It is very similar in nature but assumes an infinite flat plane.
+
+Usage
+-----
+Adding L.SimpleGraticule:
+
+```JavaScript
+var options = {interval: 20,
+               showshowOriginLabel: true,
+               redraw: 'move'};
+
+L.simpleGraticule(options).addTo(map);
+```
+####Options:####
+- interval: The spacing in map units between horizontal and vertical lines.
+- showOriginLabel: true Whether or not to show '(0,0)' at the origin.
+- redraw: on which map event to redraw the graticule. On `move` is default but `moveend` can be smoother.
+
+Notes
+-----
+- This is my first open source contribution. I appreciate feedback on any topics!
